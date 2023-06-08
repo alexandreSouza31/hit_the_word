2ce56c7ce41f40b0e56ae02889256204d57aac1f
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-08 16:12:58 -0300

    will create the words of the game

---
+++ src/data/wordsList.jsx
@@ -0,0 +1,10 @@
+
+export const wordsList = {
+    país: ["Argélia", "Quirguistão", "Islândia",
+        "Somália", "Djibouti", "Samoa","Irlânda"],
+    fruta: ["pêra", "cacau", "toranja", "pitaya",
+        "banana", "mamão", "cupuaçu"],
+    programação: ["bug", "linguagem", "biblioteca",
+        "framework", "front", "back", "fullstack",
+        "dados"]
+}
