696fd16693f19550475f4afa392f3357bef2c199
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-09 15:13:41 -0300

    will modify the component to advance

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,11 @@
 import "./StartScreen.css";
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
     return (
         <div className="start_container">
             <h1>Hit the Word</h1>
             <p>Está preparado pro desafio?</p>
-            <button>Começar jogo</button>
+            <button onClick={startGame}>Começar jogo</button>
         </div>
     )
 }
