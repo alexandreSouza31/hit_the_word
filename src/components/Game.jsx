919615ef7df7e97d6de2c517674ec355d5afc533
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-10 22:02:42 -0300

    will create the game component structure

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,31 @@
 import "./Game.css";
 
-const Game = ({endGame}) => {
+const Game = ({ endGame }) => {
     return (
-        <div>
-            <h2>Qual é a palavra?</h2>
-            <button onClick={endGame}>Terminar jogo</button>
+        <div className="game_container">
+            <p className="points">
+                <span>Pontuação: 000</span>
+            </p>
+            <h1>Adivinhe a palavra:</h1>
+            <h3 className="tip">
+                Dica sobre a palavra: <span>tal coisa</span>
+            </h3>
+            <div className="word_container">
+                <span className="letter">A</span>
+                <span className="blank_square"></span>
+            </div>
+            <div className="letter_container">
+                <p>Tente adivinhar uma letra da palavra:</p>
+                <form>
+                    <input type="text" name="letter" maxLength="1" required />
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className="wrong_letters_container">
+                <p>Letras já utilizadas:</p>
+                <span>a,</span>
+                <span>b,</span>
+            </div>
         </div>
     )
 }
