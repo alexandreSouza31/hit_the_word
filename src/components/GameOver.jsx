036920d17d54f0d0ccc3af1e99eae8026d83fe46
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-12 15:02:07 -0300

    will show the score when the game is over

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,10 @@
 import "./GameOver.css";
 
-const GameOver = ({retry}) => {
+const GameOver = ({retry,score}) => {
     return (
-        <div>
+        <div className="game_over_container">
             <h2>Fim de jogo!</h2>
+            <h3>A sua pontuação foi: <span>{score}</span></h3>
             <button autoFocus  onClick={retry}>Começar novamente</button>
         </div>
     )
